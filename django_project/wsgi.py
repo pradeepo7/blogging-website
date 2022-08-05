b145a7b19d7bc22b15eda076a71aa609da86a604
pradeep_poojary <poojarypradeep999@gmail.com>
2022-08-05 12:57:53 +0530

    changed

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
